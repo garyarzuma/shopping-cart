5a59fc9534d38978ff6c6416a209b25e5b2e7f1e
Gary <garyarzumanyan@gmail.com>
2021-01-27 14:38:19 -0800

    Added total qty number next to checkout nav link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { CartContext } from "../context/cart-context";
 
 const Nav = () => {
+  const [totalQty, setTotalQty] = useState(0);
+  const [state] = useContext(CartContext);
+
+  useEffect(() => {
+    let total = 0;
+    //sums up all the quantities in our state
+    state.carts.forEach((element) => {
+      total = element.qty + total;
+    });
+    setTotalQty(total);
+  }, [state]);
+
   return (
     <nav>
       <h3>Logo</h3>
@@ -13,7 +26,10 @@ const Nav = () => {
           <li>Products</li>
         </Link>
         <Link to="/cart">
-          <li>Cart</li>
+          <li>
+            <span>Checkout</span>
+            <div className="cart-number">{totalQty}</div>
+          </li>
         </Link>
       </ul>
     </nav>
