5f36cc4c817f8b18e9df55305b75e9c7e83951ac
Gary <garyarzumanyan@gmail.com>
2021-01-28 17:58:04 -0800

    Added background image for homepage and text

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,11 @@ const Home = () => {
   return (
     <div className="home">
       <h1>Welcome</h1>
+
+      <div className="home-container">
+        <div>Where quality</div>
+        <div>Meets fashion</div>
+      </div>
     </div>
   );
 };
