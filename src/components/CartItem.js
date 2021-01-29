8121637d41c00803e6db2eae43c1388f5e908c70
Gary <garyarzumanyan@gmail.com>
2021-01-28 21:10:08 -0800

    Styled cart items

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -12,15 +12,16 @@ const CartItem = (props) => {
   };
 
   return (
-    <div className="cartitem">
+    <div className="cart-item">
+      <div className="cart-item-qty">x{props.qty}</div>
       <img className="small-img" src={props.url} alt={props.name} />
-      <div className="cartitem-name">
-        {props.name} x{props.qty}
+      <div className="cart-container">
+        <div className="cartitem-name">{props.name}</div>
+        <div className="cartitem-price">${props.price}</div>
+        <button className="del-button" onClick={delFromCart}>
+          Delete
+        </button>
       </div>
-      <div className="cartitem-price">${props.price}</div>
-      <button className="del-button" onClick={delFromCart}>
-        Delete
-      </button>
     </div>
   );
 };
