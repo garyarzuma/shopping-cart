172ea28ceb205ea29c3e4173ee07c0edf947011d
Gary <garyarzumanyan@gmail.com>
2021-01-28 20:08:05 -0800

    Updated font

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,19 @@
 import React from "react";
-import homepagePic from "../images/homepagePic.jpg";
+import backgroundImages from "../images/Background-Shopping-Cart.png";
 
 const Home = () => {
   return (
-    <div className="home">
-      <h1>Welcome</h1>
+    <div
+      className="home"
+      style={{
+        backgroundImage: `url(${backgroundImages})`,
+      }}
+    >
+      <h1 className="welcome">Welcome</h1>
 
       <div className="home-container">
-        <div>Where quality</div>
-        <div>Meets fashion</div>
+        <div className="fancy-text">WHERE QUALITY</div>
+        <div className="fancy-text">Meets Fashion</div>
       </div>
     </div>
   );
